90dfa3c6febabdad00f2337c0d2c38f63db83278
Manan Surana <59257437+suranamanan@users.noreply.github.com>
2020-06-23 19:36:16 -0400

    footer component

--- src/Components/ProductPage/Product.js
+++ src/Components/ProductPage/Product.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Header from '../LandingPage/Header.js'
 import Navbar from '../LandingPage/Navbar.js'
+import Navbar from '../LandingPage/Footer/Footer.js'
 
 class Product extends React.Component{
 
   render(){
 
+
+    const flexBox = {
+
+      display: 'flex',
+      flexDirection: 'row'
+
+    }
 
     return(
 
@@ -14,7 +22,7 @@ class Product extends React.Component{
         <Header />
         <Navbar />
 
-        <div>
+        <div style={flexBox}>
 
           <div>
             <h5>Name of the product</h5>
@@ -23,8 +31,11 @@ class Product extends React.Component{
 
           <h6>Description of the product</h6>
 
+          <h6>Price of the product</h6>
 
         </div>
+
+        <Footer />
 
 
       </div>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Body from './Components/CategoryPage/Body'
-
-
+import Product from './Components/ProductPage/Product'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Product />
   </React.StrictMode>,
   document.getElementById('root')
 );
